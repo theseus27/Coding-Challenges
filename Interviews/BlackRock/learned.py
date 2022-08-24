6d71cf7ca585fb611bbcd404019a56ebaf885635
theseus27 <theseus.lim@gmail.com>
2022-08-24 16:56:34 -0400

    learned

--- Interviews/BlackRock/learned.py
+++ Interviews/BlackRock/learned.py
@@ -1,3 +1,24 @@
-#Splitting on character
-word = "hello.i.am.theseus"
-print(word.split("."))
+#                 GENERAL PROGRAMMING
+
+
+#                       SYNTAX
+word = "hi.i.am.theseus"
+listname = []
+word.split(".") #Splitting on character
+word.strip()    #Removing whitespace
+listname.sort(reverse=True) #Sort list
+word.capitalize() #Capitalize word
+
+import sys      #Reading in from stdin
+#for line in sys.stdin | sys.stdin.readline()
+
+
+#                       CONCEPTS
+#Negative indexing  (left side is NOT inclusive, right side IS)
+word = "hello"
+word[:-1]    # = "hell"
+word[-1:]    # = "o"
+
+#Tuples
+tupleList = [(1, True, "a"), (2, False, "b")]
+tupleList[0][1] = # True
